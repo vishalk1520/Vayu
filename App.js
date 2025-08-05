6ef74c70158180f44ac3cc24b27ec9324b5a1821
Vishal Kanojiya <vishalkanojiya1512@gmail.com>
2025-08-05 21:16:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CabBooking from "./components/CabBooking";
+
+function App() {
+  return (
+    <div>
+      <CabBooking />
+    </div>
+  );
+}
+
+export default App;
